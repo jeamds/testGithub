6cdcc913b28fa7ac475395ae216681b6a1c13356
jeamds <jeamds-amazon@yahoo.fr>
2013-01-16 14:54:08 +0100

    test1

--- src/testGit/Hello.java
+++ src/testGit/Hello.java
@@ -11,7 +11,7 @@ package testGit;
 public class Hello {
     
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello2");
     }
     
 }
